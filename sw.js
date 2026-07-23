524dd8a9abead770000f8c57ff76baeb89788dda
K-RYDE <kalleb147@hotmail.com>
2026-07-23 11:42:44 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE = 'saldocerto-v1';
+const ARQUIVOS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE).then((cache) => cache.addAll(ARQUIVOS)).catch(() => {}),
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((resposta) => resposta || fetch(event.request)),
+  );
+});
